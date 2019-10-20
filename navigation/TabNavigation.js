4d1ff37f509df74722d643c045bbbb70578add84
tonky0110 <tonky0110@gmail.com>
2019-10-20 22:55:41 +0900

    11.4 Message Navigation and Refactorying TabNavigation

--- navigation/TabNavigation.js
+++ navigation/TabNavigation.js
@@ -1,21 +1,51 @@
-import { View } from 'react-native';
+import React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { createStackNavigator } from 'react-navigation-stack';
+import Home from '../screens/Tabs/Home';
+import Notifications from '../screens/Tabs/Notifications';
+import Profile from '../screens/Tabs/Profile';
+import Search from '../screens/Tabs/Search';
 
-import Home from '../screens/Home';
-import Notifications from '../screens/Notifications';
-import Profile from '../screens/Profile';
-import Search from '../screens/Search';
+const stackFactory = (initialRoute, customConfig) =>
+	createStackNavigator({
+		InitialRoute: {
+			screen: initialRoute,
+			navigationOptions: { ...customConfig }
+		}
+	});
 
 export default createBottomTabNavigator({
-	Home,
-	Notifications,
+	Home: {
+		screen: stackFactory(Home, {
+			title: 'Home',
+			headerRight: (
+				<TouchableOpacity>
+					<Text>Hello</Text>
+				</TouchableOpacity>
+			)
+		})
+	},
+	Search: {
+		screen: stackFactory(Search, {
+			title: 'Search'
+		})
+	},
 	Add: {
 		screen: View,
 		navigationOptions: {
 			tabBarOnPress: ({ navigation }) => navigation.navigate('PhotoNavigation')
 		}
 	},
-	Profile,
-	Search
+	Notifications: {
+		screen: stackFactory(Notifications, {
+			title: 'Notifications'
+		})
+	},
+	Profile: {
+		screen: stackFactory(Profile, {
+			title: 'Profile'
+		})
+	}
 });
